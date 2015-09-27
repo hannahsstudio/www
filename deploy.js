b5a640d9602898d664c911dc1c76e8ad27b4868c
FilmKnurd <andrew@andrewelster.com>
2015-09-27 12:59:12 -0400

    Alter repo url

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ var ghpages = require('gh-pages');
 var path = require('path');
  
 ghpages.publish(path.join(__dirname, 'www'), {
-	repo: 'https://' + process.env.GH_TOKEN + '@github.com/hannahsstudio/www.git'
+	repo: 'https://' + process.env.GH_TOKEN + '@github.com/hannahsstudio/www'
 },function(err) { 
 	if (err) {
 		console.log(err); 

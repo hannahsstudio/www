0041b8ea640c86ed781613f979dc73c18993184d
Travis-CI <travis@hannahsstudio.com>
2015-12-03 02:25:30 +0000

    Updates

--- scripts/endorsements.js
+++
@@ -1,6 +0,0 @@
-(function pickEndorsement() {
-	var endorsements = document.getElementsByClassName('endorsement');
-	var randomPick = Math.floor(Math.random() * endorsements.length);
-
-	endorsements[randomPick].className = "endorsement";
-})();
